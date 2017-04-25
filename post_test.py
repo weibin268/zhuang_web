a9e9400478cbd09f34cc8a24e7f3dbb080817285
weibin268 <binweizhuang@hotmail.com>
2017-04-25 17:04:14 +0800

    aaa

--- post_test.py
+++ post_test.py
@@ -1,19 +1,24 @@
-import urllib
+#import urllib
 import urllib.request
 
-url = "http://localhost:8081/Ashx/BingoTaskHandler.ashx?action="+"DailyTask-GetTaskByDate"
+url = "http://localhost:8081/Ashx/BingoTaskHandler.ashx?action="
+action = "DailyTask-GetTaskByDate"
 
-req = urllib.request.Request(url)
-req.add_header("Content-Type","application/x-www-form-urlencoded;charset=utf-8")
+fullUrl = url + action
 
-data = urllib.parse.urlencode({"args":"{UserId:'bingosoft2',BelongDate:'2017-04-23'}"})
+req = urllib.request.Request(fullUrl)
+req.add_header(
+    "Content-Type", "application/x-www-form-urlencoded;charset=utf-8")
+
+data = urllib.parse.urlencode(
+    {"args": "{UserId:'bingosoft2',BelongDate:'2017-04-23'}"})
 data = data.encode('utf-8')
 
-res =urllib.request.urlopen(req,data)
+res = urllib.request.urlopen(req, data)
 
-fPath="./post_output.txt"
+fPath = "./post_output.txt"
 
-outFile=open(fPath,"w",encoding="utf-8")
+outFile = open(fPath, "w", encoding="utf-8")
 
 try:
     outFile.write(res.read().decode())
