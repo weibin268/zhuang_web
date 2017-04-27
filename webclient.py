2a0ad5cf727043bd0aa83cad9333b9066e831f97
weibin268 <binweizhuang@hotmail.com>
2017-04-27 14:44:00 +0800

    aa

--- webclient.py
+++ webclient.py
@@ -2,7 +2,7 @@
 import urllib.request
 
 class WebClinet:
-
+    
     url=''
 
     def __init__(self,url):
@@ -26,6 +26,9 @@ data={"args": "{UserId:'bingosoft2',BelongDate:'2017-04-23'}"}
 baseUrl='http://localhost:8081/Ashx/BingoTaskHandler.ashx?action='
 action='DailyTask-GetTaskByDate'
 json=WebClinet(baseUrl).post(action,data)
+
+with open('./post_output.txt','w',encoding='utf-8') as file:
+    file.write(json)
 
 print(json)
 
